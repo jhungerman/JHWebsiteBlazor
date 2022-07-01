2c614563f64643a02d364690a070906d9685bb4d
jhungerman <joseph_hungerman@yahoo.com>
2022-07-01 15:41:59 -0500

    Updated settings

--- JosephHungerman.UI/Program.cs
+++ JosephHungerman.UI/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using JosephHungerman.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var keyVaultEndpoint = new Uri(builder.Configuration.GetSection("VaultUri").Value);
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 await builder.Build().RunAsync();
